29957580b7c2cc2eb505560d9c1e96589f25cbe2
YasserElmor <elmor.yasser@gmail.com>
2023-05-24 23:29:42 +0300

    Set up the CommandAPIRepo concrete implementation to access PgSQL through the CommandContext

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,9 +1,14 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+// this enables us to access the values stored in appsettings.json in our code using the IConfiguration interface and accessing _configuration[KeyName]
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.AddDbContext<CommandContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, CommandAPIRepo>();
 
 var app = builder.Build();
 
